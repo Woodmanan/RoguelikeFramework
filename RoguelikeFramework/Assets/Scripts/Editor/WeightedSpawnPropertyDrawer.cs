ecfb7b1f0c9b7fb75aa3936dc4755c9f9cb52b0b
Woody <woodmanan@hotmail.com>
2022-11-26 10:30:29 +0000

    Switched monster tables to weights
    Switched monster tables to using a weighted option struct, so there is more fine-tunable control over what gets spawned. This was mostly just substitutions, as well as a quick property drawer to make them more convenient to use.

---
+++ RoguelikeFramework/Assets/Scripts/Editor/WeightedSpawnPropertyDrawer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/*
+ * Custom property drawer for the weighted spawn struct that monster tables use
+ */
+
+[CustomPropertyDrawer(typeof(WeightedSpawn))]
+public class WeightedSpawnPropertyDrawer : PropertyDrawer
+{
+    const float floatWidth = 80;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        float floatOffset = Mathf.Min(position.width / 2, floatWidth);
+        position.width -= floatOffset;
+        SerializedProperty monsterProp = property.FindPropertyRelative("toSpawn");
+        SerializedProperty weightProp = property.FindPropertyRelative("weight");
+
+        EditorGUI.ObjectField(position, monsterProp, new GUIContent(""));
+
+        position.x += position.width;
+        position.width = floatOffset;
+        weightProp.intValue = EditorGUI.IntField(position, weightProp.intValue);
+    }
+}
